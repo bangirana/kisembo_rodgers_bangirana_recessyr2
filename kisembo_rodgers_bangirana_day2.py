6e3dc782a830dad0ce9093d63778ab56c234b75c
bangirana <108082781+bangirana@users.noreply.github.com>
2024-06-07 15:27:50 +0300

    First commit

---
+++ kisembo_rodgers_bangirana_day2.py
@@ -0,0 +1,32 @@
+# Day 2 Morning Session 
+
+# Control Statements
+age = 20
+
+if age <13:
+    print("Minor's movie Discount is Shs 1000")
+
+elif age > 13 and age < 18:
+    print("Teenager movie discount is Shs 500")
+
+elif age > 18 and age <65:
+    print("Adults pay price of Shs 2000")
+
+else:
+    print("Senior adults pay of Shs 5000")
+
+
+# Loops
+# For loop
+fav_colors = ["Black", "Purple", "Magenta", "Green"]
+
+for my_color in fav_colors:
+    print(my_color)
+
+
+# While Loop 
+x = 10
+while x > 1:
+    print("x is: ", x)
+    x -= 1
+
